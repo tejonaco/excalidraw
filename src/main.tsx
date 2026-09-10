9d115977121e66566940295a6981ee647f3c7fc9
tejonaco <tejonaco@gmail.com>
2026-09-10 09:56:31 +0200

    excalidraw version update

--- src/main.tsx
+++ src/main.tsx
@@ -1,55 +1,56 @@
 import ReactDOM from "react-dom/client";
 import { Excalidraw } from "@excalidraw/excalidraw";
+import "@excalidraw/excalidraw/index.css";
 import { defaultLang, languages } from "@excalidraw/excalidraw";
-import './main.css'
+import "./main.css";
 import { useCallback, useEffect, useState } from "react";
-import { locale } from '@tauri-apps/plugin-os';
-import { getMatches } from '@tauri-apps/plugin-cli';
+import { locale } from "@tauri-apps/plugin-os";
+import { getMatches } from "@tauri-apps/plugin-cli";
 import { readTextFile } from "@tauri-apps/plugin-fs";
 import { ExcalidrawInitialDataState } from "@excalidraw/excalidraw/types/types";
-
 
 function Main() {
   const [lang, setLang] = useState(defaultLang.code);
 
   async function load() {
-
     // set locale language
-    const localeLang = await locale()
-    if (localeLang && languages.findIndex(l => l.code === localeLang) !== -1) {
-      setLang(localeLang)
+    const localeLang = await locale();
+    if (
+      localeLang &&
+      languages.findIndex((l) => l.code === localeLang) !== -1
+    ) {
+      setLang(localeLang);
     }
   }
 
-  const readFile = useCallback(async function() {
-    const matches = await getMatches()
-    const file = matches.args.file
+  const readFile = useCallback(async function () {
+    const matches = await getMatches();
+    const file = matches.args.file;
 
-    if (file.value && typeof file.value === 'string') {
-      const data: ExcalidrawInitialDataState = JSON.parse(await readTextFile(file.value))
+    if (file.value && typeof file.value === "string") {
+      const data: ExcalidrawInitialDataState = JSON.parse(
+        await readTextFile(file.value),
+      );
 
-      data.scrollToContent = true
+      data.scrollToContent = true;
 
-      return data
+      return data;
     } else {
-      return null
+      return null;
     }
-  }, [])
-
+  }, []);
 
   useEffect(() => {
-    load()
-  }, []) 
-
+    load();
+  }, []);
 
   return (
-    <div style={{ height: "100vh", width: '100vw' }}>
+    <div style={{ height: "100vh", width: "100vw" }}>
       <Excalidraw langCode={lang} initialData={readFile()} />
     </div>
-  )
+  );
 }
 
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <Main />
+  <Main />,
 );
